b99c9c7ee181a8d14a87ce8abf529273bf1b55ca
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2024-08-16 13:27:49 +0300

    Complete exercises 3.20 and 3.21

--- models/person.js
+++ models/person.js
@@ -22,7 +22,14 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    minLength: 7,
+    minLength: 8,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: (props) =>
+        `${props.value} is not a valid phone number. Number must start with format: nn- or nnn-`,
+    },
   },
 });
 
